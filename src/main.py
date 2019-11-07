9cb5064f2f88dad740f6f9bf033a65d896f6340e
Audie Ni <audie.ni@yahoo.com>
2019-11-07 02:09:52 -0500

    Capitalized TOKEN variable

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}(id:{client.user.id})')
 
-client.run(token)
+client.run(TOKEN)
