e3481a4b88805217fbc603f18f3079e44d2760c4
Audie Ni <13210233+audieni@users.noreply.github.com>
2019-11-21 14:40:08 -0500

    Revised a print statement

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
-TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
-    print('Logged in as %s(%d).' % (bot.user.name, bot.user.id))
+    print("Logged in as {}({})".format(bot.user.name, bot.user.id))
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("pong")
 
 bot.run(TOKEN)
