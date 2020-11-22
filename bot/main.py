f8ac30b6cb484df5b3933e98b55b2026694ec44e
LetsChill <74666116+LetsChill@users.noreply.github.com>
2020-11-22 08:16:22 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,20 @@
-
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="-")
-TOKEN = os.getenv("TOKEN")
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
 @bot.event()
 async def on_ready():
     await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game('Labymod')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
