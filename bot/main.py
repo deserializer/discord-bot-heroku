a7a52fd5a0f0584a68521f718b0f7006d799d466
audieni <111254997+audieni@users.noreply.github.com>
2022-10-12 17:31:57 -0400

    Updated dependency versions and added intents

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,20 @@
+import discord
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print(f'Logged in as {bot.user.name}({bot.user.id})')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("pong")
+    await ctx.send('pong')
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+if __name__ == '__main__':
+    bot.run(os.getenv('DISCORD_TOKEN'))
